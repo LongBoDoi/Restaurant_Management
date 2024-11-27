bfef0c2688ab1fabaab74eb649158544386a2c25
Manh Long <manhlongnshd2001@gmail.com>
2024-11-27 17:12:44 +0700

    Update chatbot service

--- UI/src/services/chatbotService.ts
+++ UI/src/services/chatbotService.ts
@@ -1,6 +1,7 @@
 import { ChatbotConversation, ChatbotConversationDetail } from "@/models";
 import BaseService from "./baseService";
 import { EnumChatbotSender } from "@/common/Enumeration";
+import axios from "axios";
 
 class ChatbotService extends BaseService {
     protected entityName: string = 'Chatbot';
@@ -14,78 +15,52 @@ class ChatbotService extends BaseService {
      * Lấy dữ liệu đoạn chat
      */
     async getChatbotConversation(conversationID:string) : Promise<ChatbotConversation|undefined> {
-        //FAKE DỮ LIỆU
-        //#Todo: Sửa thành API đúng
-        const conversation : ChatbotConversation|undefined = JSON.parse(sessionStorage.getItem('chatbotConversation') ?? '');
-        return new Promise((resolve:any) => {
-            setTimeout(() => {
-                resolve(conversation);
-            }, 1000);
+        const response = await this.api.get('/GetChatbotConversation', { 
+            params: {
+                conversationID: conversationID
+            }
         });
+
+        return response?.data?.Data;
     }
 
     /**
      * Tạo đoạn chat mới
      */
     async createNewConversation() : Promise<ChatbotConversation|undefined> {
-        //FAKE DỮ LIỆU
-        //#Todo: Sửa thành API đúng
-        const currentDate = new Date();
-        const conversation = {
-            ConversationID: '5474678c-7ae5-49a3-a88c-09ed105515ae',
-            Topic: 'Đặt chỗ',
-            ChatbotConversationDetails: [
-                {
-                    Sender: EnumChatbotSender.Bot,
-                    Message: 'Xin chào. Tôi có thể giúp gì cho bạn?',
-                    Timestamp: currentDate
-                } as ChatbotConversationDetail
-            ]
-        } as ChatbotConversation;
+        const response = await this.api.post('/CreateChatbotConverstion');
 
-        //#TODO: Xoá dòng này sau khi implement
-        sessionStorage.setItem('chatbotConversation', JSON.stringify(conversation));
-
-        return new Promise((resolve:any) => {
-            setTimeout(() => {
-                resolve(conversation);
-            }, 1000);
-        });
+        return response?.data?.Data;
     }
 
     /**
      * Gửi tin nhắn mới
      */
     async sendNewMessage(conversationID:string, message:string):Promise<ChatbotConversationDetail|undefined> {
-        //FAKE DỮ LIỆU
-        //#Todo: Sửa thành API đúng
         const detail:ChatbotConversationDetail = {
             ConversationID: conversationID,
             Sender: EnumChatbotSender.User,
-            Message: message,
-            Timestamp: new Date()
+            Message: message
         } as ChatbotConversationDetail;
-        return new Promise((resolve:any) => {
-            setTimeout(() => {
-                resolve(detail);
-            }, 1000);
-        });
+
+        const response = await this.api.post('/SendChatbotMessage', detail);
+        return response?.data?.Data;
     }
 
     /**
      * Lấy phản hồi từ chatbot
      */
     async getNewResponse() : Promise<ChatbotConversationDetail|undefined> {
-        const detail:ChatbotConversationDetail = {
-            Sender: EnumChatbotSender.Bot,
-            Message: 'Xin lỗi, tôi không hiểu.',
-            Timestamp: new Date()
-        } as ChatbotConversationDetail;
-        return new Promise((resolve:any) => {
-            setTimeout(() => {
-                resolve(detail);
-            }, 1000);
+        //#TODO: Thay đầu API của chat bot
+        const chatbotApiUrl = '';
+        const response = await axios.get(chatbotApiUrl, {
+            //Truyền param ở đây
+            params: {
+
+            }
         });
+
+        return response?.data;
     }
 }
 
