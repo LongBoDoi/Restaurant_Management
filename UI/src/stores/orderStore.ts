8056a076f8edbc3dbcb32af78e1a0a7e757bf883
Manh Long <Manh Long@MANHLONG>
2025-04-14 16:28:54 +0700

    update

--- UI/src/stores/orderStore.ts
+++ UI/src/stores/orderStore.ts
@@ -1,32 +1,5 @@
 import { orderService } from '@/services/orderService';
 import { createStoreOnBase } from './baseStore';
-import { MLActionResult, Order } from '@/models';
-import CommonFunction from '@/common/CommonFunction';
-import EventBus from '@/common/EventBus';
-import EventName from '@/common/EventName';
-import { EnumOrderStatus } from '@/common/Enumeration';
+import { Order } from '@/models';
 
-export const orderStore = createStoreOnBase<Order>('orderStore', orderService, {
-    actions: {
-        async getOrdersByStatus(status: EnumOrderStatus, page: number, itemsPerPage: number) {
-            try {
-                (this as any)._dataList = [];
-
-                const result:MLActionResult = await orderService.getOrdersByStatus(status, page, itemsPerPage);
-                if (result.Success) {
-                  (this as any)._dataList = result.Data.Data;
-                  (this as any)._totalCount = result.Data.TotalCount ?? 0;
-        
-                  (this as any)._selectedIndex = 0;
-                } else {
-                  EventBus.emit(EventName.ShowToastMessage, {
-                      Message: result.ErrorMsg,
-                      Type: 'error'
-                  });
-                }
-            } catch (e) {
-                CommonFunction.handleException(e);
-            }
-        },
-    }
-});
+export const orderStore = createStoreOnBase<Order>('orderStore', orderService);
